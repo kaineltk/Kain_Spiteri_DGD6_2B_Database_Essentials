a7af6445fea79e47ea3edc47af0f92a4b3a15783
kainel <kainelspiteri@hotmail.com>
2025-04-09 05:46:43 +0200

    Added further documentation

--- main.py
+++ main.py
@@ -31,12 +31,21 @@ def getAudioBucket(db):
     return AsyncIOMotorGridFSBucket(db,bucket_name="audio")
 
 
+#This endpoint is here to test vercel's connection
 @app.get("/")
 def read_root():
     return {"message": "Vercel Online"}    
 
+#This uploads a sprite to the mongo dB it only allows sprites of type PNG 
 @app.post("/upload_sprite")
 async def upload_sprite(file: UploadFile = File(...)):
+    """
+    Retrieves DB and Bucket from MongoDB
+    Verifies file type is PNG
+    Reads file content and uploads it to GridFS bucket
+    IF successful returns the uploaded item ID, else an HTTP Exception 500 
+    """
+
     db = getDB()
     fsSprites = getSpriteBucket(db) 
 
@@ -68,6 +77,14 @@ async def upload_sprite(file: UploadFile = File(...)):
 #Get Sprite
 @app.get("/spritefile/{sprite_id}")
 async def get_sprite_file(_id: str):
+    """
+    Retrieves DB and Bucket from MongoDB
+    Converts _id paramater to ObjectId 
+    Searches through files DB to find a file with the matching ID
+    If the file exists it gets it from the GridFS bucket 
+    A StreamingResponse is returned containing the file
+    """
+
     db = getDB()
     fsSprites = getSpriteBucket(db) 
 
@@ -88,6 +105,12 @@ async def get_sprite_file(_id: str):
 
 @app.get("/spritedata/{sprite_id}")
 async def get_sprite_data(_id: str):   
+    """
+    Retrieves DB from MongoDB
+    Converts _id paramater to ObjectId 
+    Searches through files DB to find a file with the matching ID
+    If the file exists it returns the file data
+    """
     db = getDB()
 
     fileID = ObjectId(_id) #Convert the sprite_id to ObjectId
@@ -107,13 +130,18 @@ async def get_sprite_data(_id: str):
 #Get All Sprites
 @app.get("/all_sprites")
 async def get_all_sprites():
+    """
+    Retrieves DB and Bucket from MongoDB
+    Searches the bucket and returns first 100 records found
+    """
+
     db = getDB()
     fsSprites = getSpriteBucket(db) 
 
-    savedSprites = await fsSprites.find().to_list(length=100)
+    savedSprites = await fsSprites.find().to_list(length=100) #Get first 100 records
     allSprites = []
 
-    for sprite in savedSprites:
+    for sprite in savedSprites: #For each record add the following data
         allSprites.append({
             "_id": str(sprite["_id"]),  #Convert ObjectId to string
             "filename": sprite["filename"],
@@ -123,23 +151,30 @@ async def get_all_sprites():
 
     return {"all_sprites": allSprites}
 
+#Upload audio
 @app.post("/upload_audio")
 async def upload_audio(file: UploadFile = File(...)):
+    """
+    Retrieves DB and Bucket from MongoDB
+    Verifies file type is audio
+    Reads file content and uploads it to GridFS bucket
+    IF successful returns the uploaded item ID, else an HTTP Exception 500 
+    """
     db = getDB()
     fsAudio = getAudioBucket(db)  
 
     try:
         if file.content_type != "audio/mpeg":
-            raise HTTPException(status_code=400, detail="Uploaded file is not an image")
+            raise HTTPException(status_code=400, detail="Uploaded file is not an audio file")
 
-        contents = await file.read()
+        contents = await file.read() #Read File Contents
         #Upload to GridFS
         fileID = await fsAudio.upload_from_stream(
             filename=file.filename,
             source=contents
         )
 
-        if(not fileID):
+        if(not fileID): #If upload failed return error message
             raise HTTPException(status_code=500, detail=str("Unable to Upload File"))
         
         return {
@@ -156,6 +191,14 @@ async def upload_audio(file: UploadFile = File(...)):
 #Get Audio
 @app.get("/audiofile/{audio_id}")
 async def get_audio_file(_id: str): 
+    """
+    Retrieves DB and Bucket from MongoDB
+    Converts _id paramater to ObjectId 
+    Searches through files DB to find a file with the matching ID
+    If the file exists it gets it from the GridFS bucket 
+    A StreamingResponse is returned containing the file
+    """
+
     db = getDB()
     fsAudio = getAudioBucket(db)     
     try:
@@ -168,13 +211,19 @@ async def get_audio_file(_id: str):
             raise HTTPException(status_code=404, detail="Unable to open Audio file")  
         return StreamingResponse(gridfsResult, media_type="audio/mpeg") #Ref: https://fastapi.tiangolo.com/advanced/custom-response/#additional-documentation
 
-    except HTTPException as httpE: #if an http exception is caught outpout it
+    except HTTPException as httpE: #if an http exception is caught output it
         raise httpE
     except Exception:
         raise HTTPException(status_code=404, detail="Audio file not found")
 
 @app.get("/audiodata/{audio_id}")
 async def get_audio_data(_id: str): 
+    """
+    Retrieves DB from MongoDB
+    Converts _id paramater to ObjectId 
+    Searches through files DB to find a file with the matching ID
+    If the file exists it returns the file data
+    """
     db = getDB()     
     fileID = ObjectId(_id) #Convert the audio_id to ObjectId
     fileData = await db.audio.files.find_one({"_id": fileID})
@@ -193,6 +242,11 @@ async def get_audio_data(_id: str):
 #Get All Audio
 @app.get("/all_audio")
 async def get_all_audio():
+    """
+    Retrieves DB and Bucket from MongoDB
+    Searches the bucket and returns first 100 records found
+    """
+
     db = getDB()
     fsAudio = getAudioBucket(db)
 
@@ -213,6 +267,12 @@ async def get_all_audio():
 #Add Player Score
 @app.post("/player_score")
 async def add_score(score: PlayerScore):
+    """
+    Retrieves DB from MongoDB
+    Creates a score dictionary 
+    Adds the score to the score DB (player_name: str,score: int)
+    """
+
     db = getDB()
     score_doc = score.dict()
 
@@ -229,6 +289,11 @@ async def add_score(score: PlayerScore):
 #Get Player Score
 @app.get("/player_score/{player_name}")
 async def get_score(player_name: str):
+    """
+    Retrieves DB from MongoDB
+    Searches the DB for a matching player_name and returns it
+    """
+
     db = getDB()
     result = await db.scores.find_one({"player_name": player_name})
 
@@ -241,6 +306,11 @@ async def get_score(player_name: str):
 
 @app.get("/all_scores")
 async def get_all_scores():
+    """
+    Retrieves DB from MongoDB
+    Searches the DB and returns the first 100 records
+    """
+
     db = getDB()
     savedScores = await db.scores.find().to_list(length=100)
     allScores = []
